11af9bfcabc8c23a3c91dd0085a6f5a518e77ee2
qingchen1203 <qingchen_job@foxmail.com>
2020-08-17 14:23:08 +0800

    Optimization function name

--- keadm/cmd/keadm/app/cmd/debug/debug.go
+++ keadm/cmd/keadm/app/cmd/debug/debug.go
@@ -20,16 +20,17 @@ import (
 )
 
 var (
-	edgeDebugLongDescription = `keadm debug command provide debug function to help diagnose the cluster`
+	edgeDebugLongDescription = `keadm help command provide debug function to help diagnose the cluster`
 )
 
-// NewEdgeDebug returns Keadm debug command.
+// NewEdgeDebug returns KubeEdge edge debug command.
 func NewEdgeDebug(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "debug",
-		Short: "Provide debug function to help diagnose the cluster.",
+		Short: "provide debug function to help diagnose the cluster",
 		Long:  edgeDebugLongDescription,
 	}
+
 	// add subCommand
 	cmd.AddCommand(NewCmdDebugGet(out, nil))
 
